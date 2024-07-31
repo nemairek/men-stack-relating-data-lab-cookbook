ae1f2ad3dc9c981f375d408997b4320b3ef7d33b
Knemaire <kannemaire@gmail.com>
2024-07-30 21:22:19 -0400

    init

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs')
+  });
+  
+
+
 module.exports = router;
